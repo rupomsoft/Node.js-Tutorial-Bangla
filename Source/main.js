4b9ebe43a31e676bba27550fca0294b1f56afc91
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-07-04 00:59:10 +0600

    ppt file name change
    ppt file name change

--- Source/main.js
+++ Source/main.js
@@ -1,7 +1,21 @@
 var http=require('http');
 
 var server=http.createServer(function (req,res) {
-    res.end("Hello World")
+
+
+    if (req.url == '/') { //check the URL of the current request
+
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // set response content
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+
+    }
+
+
+
 });
 
 server.listen(5050);
