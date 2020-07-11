a7f5eba084d9f1632b7e9f4a5e4c8f252e050555
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-07-12 00:31:48 +0600

    Tutorial 10,11,12
    Tutorial 10,11,12 PPT 9 Update

--- Source/main.js
+++ Source/main.js
@@ -4,17 +4,7 @@ var URL=require('url');
 
 var server=http.createServer(function (req,res) {
 
-  var myURL="http://rabbil.com/blog.html?year=2020&month=july";
 
-  var  myURLObj= URL.parse(myURL,true);
-
-  var myHostName=myURLObj.host;
-  var myPathName=myURLObj.pathname;
-  var mySearchName=myURLObj.search;
-
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.write(mySearchName);
-    res.end();
 
 
 });
