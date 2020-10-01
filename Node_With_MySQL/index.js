a27708bd9abedbdc44cdebe46b521008837e3c00
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-10-02 01:42:31 +0600

    32 Data Insert
    32 Data Insert

--- Node_With_MySQL/index.js
+++ Node_With_MySQL/index.js
@@ -1,12 +1,6 @@
 var mysql=require('mysql');
 
-
-var DatabaseConnectionConfig={
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"school"
-}
+var DatabaseConnectionConfig={host:"localhost", user:"root", password:"", database:"school"}
 
 var con=mysql.createConnection(DatabaseConnectionConfig);
 
@@ -15,9 +9,36 @@ con.connect(function (error) {
         console.log("Connection Fail")
     }
     else{
-        console.log("Connection Success")
+        console.log("Connection Success");
+        InsertData(con);
     }
 });
+
+
+function InsertData(con) {
+    let SQLQuery="INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Rabbil','01','Ten','017000000','Dhaka')"
+    con.query(SQLQuery,function (error) {
+        if(error){
+            console.log("Data insert fail");
+        }
+        else{
+            console.log("Data insert success");
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
