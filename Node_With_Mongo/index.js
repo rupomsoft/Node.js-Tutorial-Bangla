2ea6e50f4331a2faf1d770aa1e6071e14440032f
Rabbil Hasan <33941621+rupomsoft@users.noreply.github.com>
2020-10-13 02:42:49 +0600

    Node js with mongo
    Node js with mongo

--- Node_With_Mongo/index.js
+++ Node_With_Mongo/index.js
@@ -15,6 +15,21 @@ MongoClient.connect(URL,config,function (error,MyMongoClinet) {
         //DeleteOneItem(MyMongoClinet);
         //DeleleAllItem(MyMongoClinet);
 
+        //FindOneWithoutCondition(MyMongoClinet)
+
+        //FindOneWithCondition(MyMongoClinet);
+
+
+       // FindAllData(MyMongoClinet)
+       // FindAllDataByProjection(MyMongoClinet);
+
+       // FindAllDataByQuery(MyMongoClinet)
+
+        //FindAllDataByLimit(MyMongoClinet)
+
+        FindAllDataBySort(MyMongoClinet);
+
+
     }
 });
 
@@ -63,4 +78,77 @@ function DeleleAllItem(MyMongoClinet) {
 
 
 
+function FindOneWithoutCondition(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+    var FindObj={}
+    MyCollection.findOne(FindObj,function (error,result) {
+        console.log(result);
+    })
+}
 
+
+function FindOneWithCondition(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+    var FindObj={Roll: "05"}
+    MyCollection.findOne(FindObj,function (error,result) {
+        console.log(result);
+    })
+}
+
+
+function FindAllData(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+    MyCollection.find().toArray(function (error,result) {
+        console.log(result)
+    })
+}
+
+
+function FindAllDataByProjection(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+
+    var ItemObj={}
+    var ItemProjection={projection:{Class:"",Roll:""}}
+
+    MyCollection.find(ItemObj,ItemProjection).toArray(function (error,result) {
+        console.log(result)
+    })
+}
+
+function FindAllDataByQuery(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+
+    var Query={Roll :"01",City: "Rangpur"}
+
+    MyCollection.find(Query).toArray(function (error,result) {
+        console.log(result)
+    })
+}
+
+
+
+function FindAllDataByLimit(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+
+    MyCollection.find().limit(20).toArray(function (error,result) {
+        console.log(result)
+    })
+}
+
+
+function FindAllDataBySort(MyMongoClinet) {
+    var MyDataBase= MyMongoClinet.db("school");
+    var MyCollection= MyDataBase.collection('students');
+
+    var SortPattern={Class :-1}
+
+    MyCollection.find().sort(SortPattern).toArray(function (error,result) {
+        console.log(result)
+    })
+}
